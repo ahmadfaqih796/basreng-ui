d849bb8a598c4b4bc8ed022b4e12b614f58993eb
Ahmad Faqih Arifin <69781147+ahmadfaqih796@users.noreply.github.com>
2025-05-03 20:42:13 +0700

    hallo faqih

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hallo ini faqih ttttttttt</h1>
+      <h1>Hallo, Ahmad Faqih Arifin</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
